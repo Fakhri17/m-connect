4cec5b234842f7037594da04faaf6d4f01258e3a
Fakhri17 <fakhricoffe@gmail.com>
2023-07-15 20:28:58 +0700

    fix dashboard table user

--- resources/views/dashboard/detail-pengajuan.blade.php
+++ resources/views/dashboard/detail-pengajuan.blade.php
@@ -1,14 +1,49 @@
-{{-- Data Dari Form Pengajuan --}}
-<ul>
-    <li>{{ $data->nama }}</li>
-    <li>{{ $data->nama_pt }}</li>
-    <li>{{ $data->alamat_pt }}</li>
-    <li>{{ $data->email }}</li>
-    <li>{{ $data->no_telp }}</li>
-    <li>{{ $data->pengajuan }}</li>
-    <li>{{ $data->deskripsi }}</li>
-    <li>
-        <a href="{{ route('dashboard.statusUpdate', ['type' => 'confirmed', 'id' => $data->id]) }}">Success</a>
-        <a href="{{ route('dashboard.statusUpdate', ['type' => 'rejected', 'id' => $data->id]) }}">Rejected</a>
-    </li>
-</ul><br>
+@extends('layouts.dashboard')
+
+@section('content')
+@php
+	$status = $data->status;
+	$labelColor = '';
+	if($status == 'confirmed')
+	{
+		$labelColor = 'text-[#3FD2C7]';
+	}
+	else if($status == 'rejected')
+	{
+		$labelColor = 'text-[#F60000]';
+	}
+@endphp
+<div class="my-10">
+	<div class="container mx-auto px-10">
+		<div class="flex flex-wrap items-center mb-4">
+			<h1 class="text-5xl font-bold">{{ $data->nama_pt }}</h1>
+			@if ($data->status != "backlog")
+				<h5 class="mx-5 uppercase text-lg font-semibold {{ $labelColor }}">{{ $data->status }}</h5>			
+			@endif
+			<a class="py-1 px-4 text-xs bg-color-blue-4 border text-white rounded-md hover:bg-sky-500 ml-5" href="#">Hubungi</a>
+		</div>
+		<div>
+			<p class="mb-3">{{ $data->deskripsi }}</p>
+			<ul>
+				<li class="mb-2 text-lg font-light">Nama Pengaju : <span class="font-semibold">{{ $data->nama }}</span> </li>
+				<li class="mb-2 text-lg font-light">Alamat : <span class="font-semibold">{{ $data->alamat_pt }}</span> </li>
+				<li class="mb-2 text-lg font-light">Email : <span class="font-semibold">{{ $data->email }}</span> </li>
+				<li class="mb-2 text-lg font-light">No Telp/WA : <span class="font-semibold">{{ $data->no_telp }}</span> </li>
+				<li class="mb-2 text-lg font-light">Jenis Pengajuan : <span class="font-semibold">{{ $data->pengajuan }}</span></li>
+			</ul>
+			@if ($data->status == "backlog")
+				<div class="flex flex-wrap items-center mt-3">
+					<a class="py-1 px-4 text-sm bg-green-500 border text-white rounded-md hover:bg-green-600 mr-5" 
+						href="{{ route('dashboard.statusUpdate', ['type' => 'confirmed', 'id' => $data->id]) }}">Confirmed
+					</a>
+					<a class="py-1 px-4 text-sm bg-red-500 border text-white rounded-md hover:bg-red-600 mr-5" 
+						href="{{ route('dashboard.statusUpdate', ['type' => 'rejected', 'id' => $data->id]) }}">Rejected
+					</a>
+				</div>	
+			@endif
+		</div>
+	</div>
+</div>
+@endsection
+
+
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,26 +1,51 @@
 @extends('layouts.dashboard')
 
 @section('content')
-  {{-- Data Dari Form Pengajuan --}}
-  <form action="/logout" method="POST">
-    @csrf
-    <button type="submit">Logout</button>
-  </form>
+ 
 
-  @foreach ($data as $item)
-    <ul>
-        <li>{{ $item->nama }}</li>
-        <li>{{ $item->nama_pt }}</li>
-        <li>{{ $item->alamat_pt }}</li>
-        <li>{{ $item->email }}</li>
-        <li>{{ $item->no_telp }}</li>
-        <li>{{ $item->pengajuan }}</li>
-        <li>{{ $item->deskripsi }}</li>
-        <li>
-            <a href="/dashboard/{{ $item->id }}">Detail</a>
-        </li>
-    </ul><br>
-  @endforeach
+<div class="flex flex-col bg-white border shadow-sm rounded-xl">
+  <div class="-m-1.5 overflow-x-auto">
+    <div class="p-1.5 min-w-full inline-block align-middle">
+      <div class="border rounded-lg overflow-hidden">
+        <table class="min-w-full divide-y divide-gray-200 table-fixed">
+          <thead class="bg-gray-100">
+            <tr>
+              <th scope="col" class="px-6 py-3 text-left text-xs font-medium uppercase">Name</th>
+              <th scope="col" class="px-6 py-3 text-left text-xs font-medium uppercase">PT</th>
+              <th scope="col" class="px-6 py-3 text-left text-xs font-medium uppercase">Email</th>
+              <th scope="col" class="px-6 py-3 text-left text-xs font-medium uppercase"></th>
+            </tr>
+          </thead>
+          <tbody class="divide-y divide-gray-500 ">
+            @foreach ($data as $item)
+            @php
+                $status = $item->status;
+                $tableColor = '';
+                if($status == 'confirmed')
+                {
+                  $tableColor = 'bg-[#3FD2C7]/[.3]';
+                }
+                else if($status == 'rejected')
+                {
+                  $tableColor = 'bg-[#F60000]/[.3]';
+                }
+            @endphp
+              <tr class="{{ $tableColor }}">
+                <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800">{{ $item->nama }}</td>
+                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800">{{ $item->nama_pt }}</td>
+                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800">{{ $item->email }}</td>
+                <td class="px-6 py-4 flex flex-wrap items-center whitespace-nowrap text-sm font-medium">
+                  <a class="py-1 px-4 text-xs bg-color-blue-4 text-white rounded-md mr-2 hover:bg-sky-700" href="/dashboard/{{ $item->id }}">Detail</a>
+                  <a class="py-1 px-4 text-xs bg-color-blue-4 text-white rounded-md hover:bg-sky-700" href="#">Hubungi</a>
+                </td>
+              </tr>
+            @endforeach
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</div>
 @endsection
 
 
